b0f390267418e58e49c0b9df34ef9ab0fd939618
Daniel Mejta <daniel@mejta.net>
2020-11-06 11:01:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = (src, id = null) => {
+  if (!id) {
+    id=src;
+  }
+
+  if (Object(window.wpify_script_loader) !== window.wpify_script_loader) {
+    window.wpify_script_loader = {};
+  }
+
+  if (!(window.wpify_script_loader[id] instanceof Promise)) {
+    window.wpify_script_loader[id] = new Promise(function (resolve, reject) {
+      var fjs = document.getElementsByTagName('script')[0];
+
+      if (document.getElementById(id)) {
+        resolve();
+      }
+
+      var js = document.createElement('script');
+      js.id = id;
+      js.src = src;
+      js.async = true;
+      js.defer = true;
+      js.addEventListener('load', resolve);
+      js.addEventListener('error', e => reject(e.error));
+      js.src = src;
+      fjs.parentNode.insertBefore(js, fjs);
+    });
+  }
+
+  return window.wpify_script_loader[id];
+};
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Script loader
+
+usage:
+
+```
+const loadScript = require('wpify/script-loader');
+loadScript('https://www.a.com/b.js').then(() => console.log('script loaded'));
+```
+
